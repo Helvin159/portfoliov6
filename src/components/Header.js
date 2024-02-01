982762c766f076a598aa325a2af4a1ea10bab3d5
helvin159 <helvin159@gmail.com>
2024-02-01 15:07:57 -0500

    header styles and js updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<div className='header'>
+			<div className='header__container'>
+				<div className='header__container__title'>
+					<Link to={'/'}>Helvin Rymer</Link>
+				</div>
+			</div>
+			<div className='header__container'>
+				<nav className='header__container__nav'>
+					<ul>
+						<li>
+							<Link to='/featured'>Featured</Link>
+						</li>
+						<li>
+							<Link to='/about'>About</Link>
+						</li>
+						<li>
+							<button to='/contact'>Contact</button>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
