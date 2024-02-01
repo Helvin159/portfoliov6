d13314ff39e446d170d1b7a80093122061370811
helvin159 <helvin159@gmail.com>
2024-02-01 16:45:39 -0500

    small css changes, adds stat-copy and style

--- src/components/HomeHero/components/Statistics.js
+++ src/components/HomeHero/components/Statistics.js
@@ -4,20 +4,28 @@ const Statistics = () => {
 	return (
 		<div className='hero__container__main__stats'>
 			<div className='stat'>
-				<h3>1000+</h3>
-				<p>Projects completed</p>
+				<div className='stat__copy'>
+					<h3>1000+</h3>
+					<p>Projects completed</p>
+				</div>
 			</div>
 			<div className='stat'>
-				<h3>10+</h3>
-				<p>Years of experience</p>
+				<div className='stat__copy'>
+					<h3>10+</h3>
+					<p>Years of experience</p>
+				</div>
 			</div>
 			<div className='stat'>
-				<h3>12</h3>
-				<p>Design Accolades</p>
+				<div className='stat__copy'>
+					<h3>12</h3>
+					<p>Design Accolades</p>
+				</div>
 			</div>
 			<div className='stat'>
-				<h3>69</h3>
-				<p>Satisfied Clients</p>
+				<div className='stat__copy'>
+					<h3>69</h3>
+					<p>Satisfied Clients</p>
+				</div>
 			</div>
 		</div>
 	);
