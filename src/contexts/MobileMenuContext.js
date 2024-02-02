664a6d5e0d557beb8ec4aa2a2edd26169f6bc58d
helvin159 <helvin159@gmail.com>
2024-02-02 05:07:11 -0500

    adds responsiveness to home hero, adds mobilemenucontext

---
+++ src/contexts/MobileMenuContext.js
@@ -0,0 +1,18 @@
+import { createContext, useState } from 'react';
+
+export const MobileMenuContext = createContext({
+	isOpen: false,
+	setIsOpen: () => null,
+});
+
+export const MobileMenueProvider = ({ children }) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const value = { isOpen, setIsOpen };
+
+	return (
+		<MobileMenuContext.Provider value={value}>
+			{children}
+		</MobileMenuContext.Provider>
+	);
+};
