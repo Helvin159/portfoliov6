ae97d8ec7e13ef33cbdbad4d42f845c61aaf4c5a
helvin159 <helvin159@gmail.com>
2024-02-04 18:00:58 -0500

    adds scroll to top function on load

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { MobileMenueProvider } from './contexts/MobileMenuContext';
+import ScrollToTop from './utils/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<MobileMenueProvider>
 			<BrowserRouter>
-				<App />
+				<ScrollToTop>
+					<App />
+				</ScrollToTop>
 			</BrowserRouter>
 		</MobileMenueProvider>
 	</React.StrictMode>
