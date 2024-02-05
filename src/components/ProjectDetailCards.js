092a735c19ba848720346464a4ef14560addd3bf
helvin159 <helvin159@gmail.com>
2024-02-05 10:29:36 -0500

    adds styles, components, and some copy data (more to come), video component, and short mp4 video

--- src/components/ProjectDetailCards.js
+++ src/components/ProjectDetailCards.js
@@ -1,31 +1,49 @@
 import React from 'react';
 
-const ProjectDetailCards = ({ name, respOne, respTwo }) => {
+import imgOne from '../assets/png/project-det-one.png';
+import imgTwo from '../assets/png/project-det-two.png';
+import imgThree from '../assets/png/project-det-three.png';
+import imgFour from '../assets/png/project-det-four.png';
+import imgFive from '../assets/png/project-det-five.png';
+
+const ProjectDetailCards = ({
+	name,
+	respOne,
+	respOneTitle,
+	respTwo,
+	respTwoTitle,
+}) => {
 	return (
 		<div className='project__cards'>
-			<div>
-				<div>
-					<div>
-						<img src='' alt={name} />
+			<div className='project__cards__container'>
+				<div className='project__cards__container__card'>
+					<div className='project__cards__container__card__img'>
+						<img src={imgFour} alt={name} />
 					</div>
-					<p>{respOne}</p>
+					<div className='project__cards__container__card__summary'>
+						<h3>{`About ${respOneTitle}`}</h3>
+						<p>{respOne}</p>
+					</div>
 				</div>
-				<div>
-					<div>
-						<img src='' alt={name} />
+				<div className='project__cards__container__card'>
+					<div className='project__cards__container__card__img'>
+						<img src={imgFive} alt={name} />
 					</div>
-					<p>{respTwo}</p>
+					<div className='project__cards__container__card__summary'>
+						<h3>{`About ${respTwoTitle}`}</h3>
+						<p>{respTwo}</p>
+					</div>
 				</div>
 			</div>
-			<div>
-				<div>
-					<img src='' alt='' />
+			<div className='project__cards__gallery'>
+				<div className='project__cards__gallery__card'>
+					<img src={imgOne} alt={name} />
 				</div>
-				<div>
-					<img src='' alt='' />
+				<div className='project__cards__gallery__card'>
+					<img src={imgTwo} alt={name} />
 				</div>
-				<div>
-					<img src='' alt='' />
+				<div className='project__cards__gallery__card'>
+					<img src={imgThree} alt={name} />
 				</div>
 			</div>
 		</div>
--- src/components/ProjectDetailVideo.js
+++ src/components/ProjectDetailVideo.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import blogVid from '../assets/video/blogbyrollbyadp.MP4';
+
 const ProjectDetailVideo = () => {
 	return (
-		<div>
-			<h3>Video</h3>
-			<div>
-				<video>
-					<source />
+		<div className='project__video'>
+			<div className='project__video__header'>
+				<h3>Video presentation</h3>
+			</div>
+			<div className='project__video__player'>
+				<video poster='' autoplay controls='true'>
+					<source src={blogVid} type='video/mp4' />
 				</video>
 			</div>
 		</div>
