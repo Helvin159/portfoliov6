00f7639504ca61a43d7004d23189c8ea4d8f1004
helvin159 <helvin159@gmail.com>
2024-02-08 12:26:01 -0500

    adds .env variables, new styles, working on form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,23 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
-import React from 'react';
+import React, { useRef } from 'react';
 import linkedInImg from '../assets/svg/icon-linkedin.svg';
 import instagramImg from '../assets/svg/icon-instagram.svg';
 
 const Footer = ({ name, email, linkedIn, instagram, github }) => {
 	const loadingText = 'Loading...';
+	const fName = useRef();
+	const fEmail = useRef();
+	const fMsg = useRef();
+	const fPjType = useRef();
+
+	const action = (e) => {
+		e.preventDefault();
+		console.log(fName.current.value);
+		let arr = ['1', '2'];
+
+		console.log(typeof arr);
+	};
+
 	return (
 		<div className='footer'>
 			<div className='footer__contact__container'>
@@ -18,27 +31,34 @@ const Footer = ({ name, email, linkedIn, instagram, github }) => {
 							id='name'
 							name='name'
 							placeholder='YOUR NAME *'
+							ref={fName}
 						/>
 						<input
 							type='email'
 							id='email'
 							name='email'
 							placeholder='YOUR EMAIL *'
+							ref={fEmail}
 						/>
 						<input
 							type='text'
 							id='pjType'
 							name='pjType'
 							placeholder='WHAT TYPE OF PROJECT ARE YOU LOOKING FOR?'
+							ref={fPjType}
 						/>
 						<input
 							type='text'
 							id='msg'
 							name='msg'
 							placeholder='WRITE A MESSAGE...'
+							ref={fMsg}
 						/>
 					</form>
-					<button onClick={(e) => e.preventDefault()}>send</button>
+
+					<button type='submit' onClick={action}>
+						send
+					</button>
 				</div>
 				<div className='footer__contact__container__contact footer__contact__container__sizing'>
 					<div className='footer__contact__container__contact__header'>
