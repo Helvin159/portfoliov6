eab450577b2e34abbf009528f3f4a0e70eb91746
helvin159 <helvin159@gmail.com>
2024-02-10 21:01:16 -0500

    adds mobile menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { MobileMenuContext } from '../contexts/MobileMenuContext';
+
+import openBtn from '../assets/svg/icon-hamburger.svg';
+import closeBtn from '../assets/svg/icon-close.svg';
 
 const Header = ({ lastName }) => {
 	const navigate = useNavigate();
 	const handleOnClick = () => navigate('/contact');
+
+	const { isOpen, setIsOpen } = useContext(MobileMenuContext);
+
+	const handleMobileMenu = (e) => {
+		e.preventDefault();
+
+		setIsOpen(!isOpen);
+	};
 
 	return (
 		<div className='header'>
@@ -28,19 +40,33 @@ const Header = ({ lastName }) => {
 								</li>
 							</ul>
 						</nav>
-						<nav className='header__content__container__navs__mobile'>
-							<ul>
-								<li>
-									<Link to='/featured'>Projects</Link>
-								</li>
-								<li>
-									<Link to='/about'>About</Link>
-								</li>
-								<li>
-									<button onClick={handleOnClick}>Contact</button>
-								</li>
-							</ul>
-						</nav>
+						{isOpen && (
+							<nav className='header__content__container__navs__mobile'>
+								<button onClick={handleMobileMenu} className='close-btn'>
+									<img src={closeBtn} alt='Close button' />
+								</button>
+								<ul>
+									{window.location.pathname.toString() !== '/' && (
+										<li>
+											<Link to='/'>Home</Link>
+										</li>
+									)}
+									<li>
+										<Link to='/featured'>Projects</Link>
+									</li>
+									<li>
+										<Link to='/about'>About</Link>
+									</li>
+									<li>
+										<button onClick={handleOnClick}>Contact</button>
+									</li>
+								</ul>
+							</nav>
+						)}
+
+						<button className='open-btn' onClick={handleMobileMenu}>
+							<img src={openBtn} alt='Open menu' />
+						</button>
 					</div>
 				</div>
 			</div>
